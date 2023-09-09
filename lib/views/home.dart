1f86077f65d8ba2320b9d94b65b5bdfeaccb9ce4
Waqas Ahmed <wa02263@gmail.com>
2023-09-09 02:46:07 -0700

    completed

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:weather/views/detail_forecast.dart';
 import 'package:weather/view models/home_viewmodel.dart';
 import 'package:weather/widgets/feels_like_container.dart';
 
